2785912d8651dee63304dd3fc5a0ee2da4899ef3
Shonennn <Shonennn@users.noreply.github.com>
2022-11-01 12:04:08 -0400

    Nan: added user models 11.1

--- Medica/clinic/login/models.py
+++ Medica/clinic/login/models.py
@@ -3,6 +3,3 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
-    id = models.UUIDField(max_length=10)
-    pub_date = models.DateTimeField('date published')
