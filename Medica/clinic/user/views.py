acac2abcf70ade8d148e89e39bd76d82ddcb75e7
Shonennn <Shonennn@users.noreply.github.com>
2022-11-05 23:35:38 -0400

    added view for payment

--- Medica/clinic/user/views.py
+++ Medica/clinic/user/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from user.models import Payment
 # Create your views here.
 
 
 def payment(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email=request.POST.get('email')
+        value = request.POST.get('value')
+        doctor = request.POST.get('email')
+        order=Payment()
+        order.username=username
+        order.email=email
+        order.value=value
+        order.doctor=doctor
+        order.save()
     return render(request, 'payment.html', {})
 
 def doctor(request):
