399268bcf8abfb8dc64fdf22fec485868b528578
Shonennn <Shonennn@users.noreply.github.com>
2022-11-07 12:40:41 -0500

    Setting updated

--- Medica/clinic/clinic/settings.py
+++ Medica/clinic/clinic/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'medica',
         'USER': 'root',
-        'PASSWORD': 'qi990428',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
