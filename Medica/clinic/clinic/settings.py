0cae097ce66f0a1ee65de8a15e0a33b58c4621f2
Shonennn <Shonennn@users.noreply.github.com>
2022-11-14 01:29:03 -0500

    updated nginx

--- Medica/clinic/clinic/settings.py
+++ Medica/clinic/clinic/settings.py
@@ -129,7 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+ # STATIC ROOT 和 STATIC URL
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+ 
+ # MEDIA ROOT 和 MEDIA URL
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Medica/clinic/start.sh
+++ Medica/clinic/start.sh
@@ -1,9 +1,10 @@
- #!/bin/bash
- # 从第一行到最后一行分别表示：
- # 1. 生成数据库迁移文件
- # 2. 根据数据库迁移文件来修改数据库
- # 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
- python manage.py makemigrations&&
- python manage.py migrate&&
- uwsgi --ini /var/www/html/mysite2/uwsgi.ini
- # python manage.py runserver 0.0.0.0:8000
+#!/bin/bash
+# 从第一行到最后一行分别表示：
+# 1. 收集静态文件到根目录
+# 2. 生产数据库迁移文件
+# 3. 根据数据库迁移文件来修改数据库
+# 4. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
+python manage.py collectstatic --noinput&&
+python manage.py makemigrations&&
+python manage.py migrate&&
+uwsgi --ini /var/www/html/mysite3/uwsgi.ini
