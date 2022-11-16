be7f5fe8da62dfc175e7c874156d454511054ba5
Jessica9126 <912675127@qq.com>
2022-11-15 22:45:51 -0500

    start.sh edit

--- Medica/clinic/start.sh
+++ Medica/clinic/start.sh
@@ -3,7 +3,7 @@
 # 1. 收集静态文件到根目录
 # 2. 生产数据库迁移文件
 # 3. 根据数据库迁移文件来修改数据库
-# 4. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
+# 4. 用 uwsgi启动 django 服务, 不使用python manage.py runserver
 python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
 python manage.py migrate&&
